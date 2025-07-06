693203b535384be2f335aa89fb531e8aaf49582b
quannic <138555149+quannic@users.noreply.github.com>
2025-07-06 01:23:31 -0400

    Update FiguraGeometrica.java

--- ProjectoLogin/src/logic/FiguraGeometrica.java
+++ ProjectoLogin/src/logic/FiguraGeometrica.java
@@ -2,8 +2,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-//Javier Vera M. 21.697.880-3
+*/
+
 package logic;
 public class FiguraGeometrica {
     private double ladoA;
